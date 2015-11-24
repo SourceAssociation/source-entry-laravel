5057e2269d351dbf6e314f669e97b88a944e2855
ArronYR <yangyun4814@gmail.com>
2015-11-24 18:12:45 +0800

    增加params，layouts，uikit

---
+++ config/params.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* 一些常用转换
+* @author Arron <yangyun4814@gmail.com>
+* @link http://blog.helloarron.com
+*/
+
+return [
+    'copy' => '索思科技协会',
+    'power_by' => [
+        'author' => 'Arron',
+        'email' => 'yangyun4814@gmail.com',
+        'link' => 'http://blog.helloarron.com',
+    ],
+    'title' => '招新报名系统',
+
+    'is_active' => [
+        0 => '未激活',
+        1 => '激活'
+    ],
+];
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+
+    <title>Laravel 5 起步</title>
+</head>
+<body>
+<div id="wrapper">
+
+    @yield('content')
+
+    <nav class="nav-container group" id="nav-footer">
+        <div class="nav-wrap">
+            <ul class="nav container group">
+                <li class="menu-item">
+                    <a href="/" rel="nofollow" target="_blank">Laravel 5 Blog</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</body>
+</html>
